af96f3ba8218005864953d80cb70891ab993d793
Jin dy <vip.fsafds@mail.ru>
2024-03-12 19:25:20 +0300

    py-fl

---
+++ python_IN_C/lesson.py
@@ -0,0 +1,145 @@
+import numpy as np 
+from collections import deque
+
+# matrix = np.array([[1,2,3], [4,5,6], [7,8,9]])
+
+# targ_ell = 1 
+# incl = np.where(matrix == targ_ell)
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+matrix_5x5 = [
+    [1,  2,  3,  4,  5],
+    [6,  7,  8,  9,  10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25]
+]
+
+empty_matrix = []
+
+# def find_max_ell(matrixf) -> int:
+#     if len(matrixf) == 0:
+#         return "Matrix - empty"
+    
+#     max_ell = matrixf[0][0]
+#     for row in matrixf:
+#         for element in row:
+#             if element > max_ell:
+#                 max_ell = element
+#     return max_ell
+
+# print(find_max_ell(matrixf=empty_matrix))
+
+# def diagonal_and_secondary_sum(matrix: list):
+#     if len(matrix) == 0:
+#         return "Matrix - empty"
+    
+#     matrix_main_diagonal = 0
+#     matrix_secondary_diagonal = 0
+#     step_row = 0
+#     step_element = 0
+
+#     for row in matrix:
+#         matrix_main_diagonal += matrix[step_row][step_element]
+#         matrix_secondary_diagonal += matrix[step_row][-step_element - 1]
+#         step_row += 1 
+#         step_element += 1
+
+#     return matrix_main_diagonal, matrix_secondary_diagonal
+
+# print(diagonal_and_secondary_sum(matrix=matrix_5x5))
+
+
+
+
+# def find_exsit(matrix: list):
+#     if len(matrix) == 0:
+#         return "Matrix - empry"
+    
+    
+#     return 
+
+
+# Графы 
+
+# def bfs(graph, start):
+#     visited = set()
+#     queue = deque([start])
+
+#     while queue:
+#         current = queue.popleft()
+#         visited.add(current)
+
+#         for neighbor in graph[current]:
+#             if neighbor not in visited:
+#                 queue.append(neighbor)
+    
+#     print(visited)
+
+
+# graph = {
+#     'A': ['B', 'C'],
+#     'B': ['A', 'D', 'E'],
+#     'C': ['A', 'F', 'G'],
+#     'D': ['B'],
+#     'E': ['B'],
+#     'F': ['C'],
+#     'G': ['C']
+# }
+
+# bfs(graph, 'A')
+
+
+# def dfs(graph, start, visited=None):
+#     if visited is None:
+#         visited = set()
+#     visited.add(start)
+#     print(start)
+#     for neighbor in graph[start]:
+#         if neighbor not in visited:
+#             dfs(graph, neighbor, visited)
+
+
+# graph = {
+#     1: [2, 3],
+#     2: [1, 4, 5],
+#     3: [1, 6, 7],
+#     4: [2],
+#     5: [2],
+#     6: [3],
+#     7: [3]
+# }
+
+# dfs(graph, 7)
+
+
+def generateMatrix(n: int):
+        matrix = [[0 for _ in range(n)] for _ in range(n)]
+
+        coordinate_x = 0
+        coordinate_y = 0
+
+        int_for_matrix = 1
+        for i, row in enumerate(matrix):
+            for j, element in enumerate(row):
+                
+                if element == 0:
+                    matrix[coordinate_x][coordinate_y] = int_for_matrix
+                    int_for_matrix += 1
+                if len(matrix) > i:
+                       coordinate_x += 1
+                elif len(matrix) == i:
+                     continue
+        
+         
+        print(len(matrix))
+        print(len(matrix[0]))
+        print(matrix)
+
+generateMatrix(3)
---
+++ python_IN_C/main.py
@@ -0,0 +1,300 @@
+import random
+
+# def isPalindrome(x: int) -> bool:
+#     first_list = []
+#     second_list = []
+    
+#     for el in str(x):
+#         first_list.append(el)
+
+
+#     for el in str(x)[::-1]:
+#         second_list.append(el)
+
+#     print(first_list == second_list)
+
+# isPalindrome(x= "")
+
+
+# def romanToInt(s: str) -> int:
+#     if s == None:
+#         return 0
+    
+#     final_int = 0
+    
+#     for el in s:
+#         if el == 'I':
+#             final_int += 1
+#         elif el == "V":
+#             final_int += 5
+#         elif el == "X":
+#             final_int += 10
+#         elif el == "L":
+#             final_int += 50
+#         elif el == "C":
+#             final_int += 100
+#         elif el == "D":
+#             final_int += 500
+#         elif el == "M":
+#             final_int += 1000
+
+#     return final_int
+
+# romanToInt(s= "III")
+
+# print(romanToInt(s= "III"))
+# print(romanToInt(s= "LVIII"))
+# print(romanToInt(s= "MCMXCIV"))
+
+
+# def romanToInt(s: str) -> int:
+#     m = {
+#                 'I': 1,
+#                 'V': 5,
+#                 'X': 10,
+#                 'L': 50,
+#                 'C': 100,
+#                 'D': 500,
+#                 'M': 1000
+#             }
+    
+#     total_cost = 0
+
+#     for i in range(len(s)):
+#         if i < len(s) - 1 and m[s[i]] < m[s[i+1]]:
+#             total_cost -= m[s[i]]
+#         else:
+#             total_cost += m[s[i]]
+    
+#     return total_cost
+
+# print(romanToInt(s="MCMXCIV"))
+
+# nums1 = [1,1,2,2]
+# nums2 = [0,0,1,1,1,2,2,3,3,4]
+
+# def removeDuplicates(nums) -> int:
+#     main_list = []
+#     for i in range(len(nums) - 1):
+#         if nums[i] != nums[i + 1]:
+#             main_list.append(nums[i])
+    
+#     main_list.append(nums[-1])
+
+#     return main_list
+
+
+# print(removeDuplicates(nums=nums2))
+
+
+# st = ["flower","flow","flight"]
+# s = ["dog","racecar","car"]
+
+# def longestCommonPrefix(strs) -> str:
+#     if not strs:
+#         return ""
+    
+#     for i in range(len(strs[0])):
+#         for srtring in strs[1:]:
+#             if i >= len(srtring) or srtring[i] != strs[0][i]:
+#                 return strs[0][:i]
+
+#     return strs[0]
+
+# longestCommonPrefix(st)
+
+# longestCommonPrefix(s)
+
+# nums1 = [1,3,5,6,8,10]
+
+# def searchInsert(nums: list, target: int) -> int:
+
+#     if target not in nums:
+#         nums.append(target)
+#         nums.sort()
+#         return nums.index(target)
+#     else:
+#         return nums.index(target)
+
+
+
+
+# searchInsert(nums=nums1, target=7)
+
+# print(nums1)
+
+
+# Пример 1:
+
+# s1 = "()"
+# #  Вывод: true
+# # Пример 2:
+
+# s2 = "()[]{}"
+# #  Вывод: true
+# # Пример 3:
+
+# s3 = "(]"
+# #  Вывод: ложь
+
+# s4 = "{[]}"
+
+
+# def isValid( s: str) -> bool:
+#     for el in range(len(s)):
+#         if s[el] == "(" and s[el+1] != ")":
+#             return False
+#         elif s[el] == "[" and s[el+1] != "]":
+#             return False
+#         elif s[el] == "{" and s[el+1] != "}":
+#             return False
+#         else:
+#             return True
+
+#     s = sorted(s)
+#     s = ''.join(s)
+
+#     for el in range(len(s)):
+#         if s[el] == "(" and s[el+1] == ")":
+#             return True
+#         elif s[el] == "[" and s[el+1] == "]":
+#             return True
+#         elif s[el] == "{" and s[el+1] == "}":
+#             return True
+#         else:
+#             return False
+        
+
+        # if s[el] == simbol_list[1] and s[el+1] == simbol_list[2]:
+        #     print('Verno')
+        # if s[el] == simbol_list[3] and s[el+1] == simbol_list[4]:
+        #     print('Verno')
+        # if s[el] == simbol_list[5] and s[el+1] == simbol_list[6]:
+        #     print('Verno')
+
+        
+       
+# print(sorted('{[]}'))
+# print(sorted("()[]{}"))
+# print(sorted(s3))
+
+# isValid(s=s1)
+# isValid(s=s2)
+# isValid(s=s3)
+# isValid(s=s4)
+
+# simbol_list = {
+#         1: "(",
+#         2: ")",
+#         3: "[",
+#         4: "]",
+#         5: "}",
+#         6: "{",
+#     }
+
+# print(simbol_list[1] != simbol_list[2])
+
+
+
+# def twoSum( nums: int, target: int):
+#         hash = {}
+#         for i in range(len(nums)):
+#             complement = target - nums[i]
+#             if complement in hash:
+#                 return [i, hash[complement]]
+#             hash[nums[i]] = i
+
+
+# twoSum([1,2,5,3,6,3,6,2,7,11,15], 9)
+
+# test_str = "luffy is still joyboy"
+
+# def lengthOfLastWord(s: str) -> int:
+#     is_open = True
+#     sum_el = 0
+#     for el in s[::-1]:
+#         if el == " " and is_open:
+#             continue
+#         if el.isalpha():
+#             sum_el += len(el)
+#             is_open = False
+#         elif el == " ":
+#             break
+            
+#     return sum_el
+
+# lengthOfLastWord(test_str)
+# print(lengthOfLastWord(test_str))
+
+
+# def strStr(haystack: str, needle: str) -> int:
+#     if needle in haystack:
+#         index_ellement = haystack.find(needle)
+#         return index_ellement
+#     else:
+#         return -1
+
+# strStr(haystack="hello", needle="ll")
+
+# Входные данные: кандидаты = [2,3,6,7], цель = 7
+#  Выходные данные: [[2,2,3],[7]]
+
+# Входные данные: кандидаты = [2,3,5], цель = 8
+#  Выходные данные: [[2,2,2,2],[2,3,3],[3,5]]
+
+
+def combinationSum(candidates: int, target: int):
+    all_combinations = []
+    for el in range(len(candidates)):
+        return
+    
+
+# Ввод: числа = [2,0,2,1,1,0]
+# Вывод: [0,0,1,1,2,2]
+
+
+
+# nums = [1,2,3,4,5,6,7,8,9,0]
+
+# def permute(nums):
+     
+#      result_stack = []
+#      combination_stack = 0
+
+#      i = 0
+#      while(i < len(nums)):      
+#         combination_stack += len(nums) * len(nums) - i
+#         i += 1
+
+#      combination_stack = combination_stack/len(nums) - 3
+#      while(combination_stack >= len(result_stack)):
+#         randol_ellement = nums[:]
+#         random.shuffle(randol_ellement)
+#         if randol_ellement not in result_stack:
+#             result_stack.append(randol_ellement)
+    
+#      return result_stack
+
+
+# print(permute(nums))
+
+
+# nums = [1,2,3]
+
+# def permute(nums):
+#     def backtrack(nums, path):
+#         if not nums:
+#             result.append(path)
+#             return
+#     for i in range(len(nums)):
+#         backtrack(nums[:i] + nums[i+1], path + nums[i])
+#         result = []
+#         backtrack(nums, [])
+#         return result
+
+
+# permute(nums)
+
+
+
+
